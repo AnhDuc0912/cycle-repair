f797f22702f4ab4d86e0cb3b3197bc56c3c73605
KLIUH <lykhanh0610203@gmail.com>
2023-12-18 22:14:22 +0700

    booking page 1st

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/cycle_repair_web');
+        await mongoose.connect('mongodb+srv://dg1710dfkgard:41UL686Nu6WFeenu@cluster0.fhxpvhb.mongodb.net/cycle_repair_web');
     } catch (error) {
         console.log('Connect Fail');
         console.log(error);
